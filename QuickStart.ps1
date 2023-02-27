3c7a4c72b66dfb9d23192324e926784eee0debf9
AnalogCyan <git@thayn.me>
2023-02-27 14:28:38 -0600

    Initial version of QuickStart script for testing.

---
+++ QuickStart.ps1
@@ -0,0 +1,63 @@
+#Requires -RunAsAdministrator
+
+function Show-Message {
+  param (
+    [Parameter(Mandatory = $false)]
+    [Switch]
+    $NoNewline,
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [String[]]$MessageType,
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [String[]]$MessageText
+  )
+  switch ($MessageType) {
+    'info' {
+      Write-Host -NoNewline -f Blue "[INFO] "
+      if ($NoNewline) { Write-Host -NoNewline $MessageText }
+      else { Write-Host $MessageText }
+    }
+    'notice' {
+      Write-Host -NoNewline -f DarkYellow "[NOTICE] "
+      if ($NoNewline) { Write-Host -NoNewline $MessageText }
+      else { Write-Host $MessageText }
+    }
+    'warn' {
+      Write-Host -NoNewline -f Red "[WARN] "
+      if ($NoNewline) { Write-Host -NoNewline $MessageText }
+      else { Write-Host $MessageText }
+    }
+    'error' {
+      Write-Host -NoNewline -f Red "[ERROR] "
+      if ($NoNewline) { Write-Host -NoNewline $MessageText }
+      else { Write-Host $MessageText }
+    }
+    'success' {
+      Write-Host -NoNewline -f Green "[DONE] "
+      if ($NoNewline) { Write-Host -NoNewline $MessageText }
+      else { Write-Host $MessageText }
+    }
+  }
+  Start-Sleep -Seconds 1
+}
+
+if ($(Get-Command pwsh.exe -ErrorAction SilentlyContinue) -and $(Get-Command wt.exe -ErrorAction SilentlyContinue) -and $(Get-Command git.exe -ErrorAction SilentlyContinue) -and $(Get-Command sudo.exe -ErrorAction SilentlyContinue)) {
+  git.exe clone 'https://github.com/AnalogCyan/SwiftWin.git'
+  Set-ExecutionPolicy Bypass -Scope Process -Force
+  sudo.exe ./SwiftWin/SwiftWin.ps1
+}
+else {
+  if (Get-Command winget.exe -ErrorAction SilentlyContinue) {
+    $Utils = "9MZ1SNWT0N5D", "9MZ1SNWT0N5D", "Git.Git", "gerardog.gsudo"
+    foreach ($Util in $Utils) {
+      winget install --id "$Util" --silent --force  --accept-package-agreements --accept-source-agreements
+    }
+    #wt.exe 'pwsh.exe -Command "irm sw.thayn.xyz | iex"'
+    wt.exe 'pwsh.exe -Command "./QuickStart.ps1"'
+  }
+  else {
+    Show-Message -MessageType "error" -MessageText "Winget not found, please manually install updates from Windows Update & Microsoft Store and try again."
+    exit
+  }
+}
